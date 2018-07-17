96a9c33e5eb5ade1636f29313fc9c6cb35bc490d
Purple Myst <PurpleMyst@users.noreply.github.com>
2018-07-17 11:57:16 +0200

    Recall draw on resize

--- pre.js
+++ pre.js
@@ -1,5 +1,5 @@
-/* jshint browser: true, devel: true, undef: true */
-/* global Module, ImageData, Uint8Array, Uint8ClampedArray */
+/* jshint browser: true, devel: true, undef: true, typed: true */
+/* global ImageData, Module */
 
 Module.onRuntimeInitialized = function() {
     "use strict";
@@ -7,11 +7,8 @@ Module.onRuntimeInitialized = function() {
     var draw_mandelbrot = Module.cwrap("draw_mandelbrot", "void", ["size_t", "size_t", "[uint8_t]"]);
 
     var canvas = document.createElement("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    document.body.appendChild(canvas);
-
     var ctx = canvas.getContext("2d");
+    document.body.appendChild(canvas);
 
     function allocate_uint8_array(buffer_size) {
         var buffer_offset = Module._malloc(buffer_size);
@@ -25,12 +22,19 @@ Module.onRuntimeInitialized = function() {
         };
     }
 
-    var rgba_pixels = allocate_uint8_array(canvas.width * canvas.height * 4);
-    console.time("drawing mandelbrot");
-    draw_mandelbrot(canvas.width, canvas.height, rgba_pixels.offset);
-    console.timeEnd("drawing mandelbrot");
+    function draw() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        var rgba_pixels = allocate_uint8_array(canvas.width * canvas.height * 4);
+        console.time("drawing mandelbrot");
+        draw_mandelbrot(canvas.width, canvas.height, rgba_pixels.offset);
+        console.timeEnd("drawing mandelbrot");
 
-    ctx.putImageData(new ImageData(new Uint8ClampedArray(rgba_pixels.data), canvas.width, canvas.height), 0, 0);
+        ctx.putImageData(new ImageData(new Uint8ClampedArray(rgba_pixels.data), canvas.width, canvas.height), 0, 0);
 
-    Module._free(rgba_pixels.offset);
+        Module._free(rgba_pixels.offset);
+    }
+
+    draw();
+    window.addEventListener("resize", draw);
 };
